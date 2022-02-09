9b27a7b12fa1240ea5870f2bb092527953789076
Andrew Millar <andrew@elgentos.nl>
2022-02-09 15:04:33 +0100

    Add a removeCoupon page-object function, reformat the cart spec file, fix the cart page-object fixtures references, add json file extentions

--- cypress/page-objects/hyva/cart.js
+++ cypress/page-objects/hyva/cart.js
@@ -1,16 +1,24 @@
-import cart from "../../fixtures/hyva/selectors/cart"
+import cart from "../../fixtures/hyva/selectors/cart.json";
 
 export class Cart {
     static addProductToCart(url) {
-        cy.visit(url)
-        cy.get(cart.product.addToCartButton).click()
+        cy.visit(url);
+        cy.get(cart.product.addToCartButton).click();
     }
 
-    static addCouponCode() {
-        cy.visit(cart.url.cartUrl)
-        cy.get(cart.cart.couponDropdownSelector).click()
-        cy.get(cart.cart.couponInputField).type(cart.cart.couponCode)
-        cy.get(cart.cart.addCouponButton).click()
-        cy.wait(2000)
+    static addCouponCode(couponCode) {
+        cy.visit(cart.url.cartUrl);
+        cy.get(cart.couponDropdownSelector).click();
+        cy.get(cart.couponInputField).type(couponCode);
+        cy.get(cart.addCouponButton).click();
+        cy.wait(2000);
+    }
+
+    static removeCoupon() {
+        cy.visit(cart.url.cartUrl);
+        cy.get(cart.couponDropdownSelector).click();
+        cy.get(cart.addCouponButton).click({
+            force: true,
+        });
     }
 }
