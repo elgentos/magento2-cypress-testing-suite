de596efd034c671b434037b31c98deeb83baae6f
vladhorielov <97949102+vladhorielov@users.noreply.github.com>
2022-02-18 14:29:34 +0200

    #25 added mobile test for Hyva

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,3 +1,7 @@
 export const isMobile = () => {
     return Cypress.config("viewportWidth") < Cypress.env("mobileViewportWidthBreakpoint");
 };
+
+export const isMobileHyva = () => {
+    return Cypress.config("viewportWidth") < Cypress.env("mobileViewportWidthBreakpointHyva");
+};
