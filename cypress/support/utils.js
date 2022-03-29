79b80350702e6bb85a0615a031cc9a69ef128e8d
klg <k.langenberg@imi.de>
2022-03-29 08:41:02 +0200

    feat: create coupon code via Magento REST api

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,7 +1,21 @@
 export const isMobile = () => {
-    return Cypress.config("viewportWidth") < Cypress.env("mobileViewportWidthBreakpoint");
-};
+    return Cypress.config('viewportWidth') < Cypress.env('mobileViewportWidthBreakpoint')
+}
 
 export const isMobileHyva = () => {
-    return Cypress.config("viewportWidth") < Cypress.env("mobileViewportWidthBreakpointHyva");
-};
+    return Cypress.config('viewportWidth') < Cypress.env('mobileViewportWidthBreakpointHyva')
+}
+
+export function getWebsites() {
+    return cy.request({
+        method: 'GET',
+        url: '/rest/V1/store/websites',
+        headers: {
+            authorization: `Bearer ${Cypress.env('MAGENTO2_ADMIN_TOKEN')}`
+        },
+        timeout: 100000,
+    })
+        .then(response => {
+            return response.body
+        })
+}
