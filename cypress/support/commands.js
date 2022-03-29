f9700d44f685f0a59d6365d657f6adf95d520a50
klg <k.langenberg@imi.de>
2022-03-29 09:15:39 +0200

    feat: add commands to assert success messages, error messages and page titles

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,5 @@
 import 'cypress-localstorage-commands'
+import globalSelectors from '../fixtures/globalSelectors.json'
 
 Cypress.Commands.add('login', (email, pw) => {
     it('Test login', () => {
@@ -7,4 +8,22 @@ Cypress.Commands.add('login', (email, pw) => {
         cy.get('#pass').type(`${pw}{enter}`, {force:true})
         //cy.get('#login-form').contains('Inloggen').click({force:true})
     })
+})
+
+Cypress.Commands.add('shouldHavePageTitle', title => {
+    cy.get(globalSelectors.pageTitle)
+        .should('exist')
+        .should('contain.text', title)
+})
+
+Cypress.Commands.add('shouldHaveSuccessMessage', message => {
+    cy.get(globalSelectors.successMessage)
+        .should('exist')
+        .should('contain.text', message)
+})
+
+Cypress.Commands.add('shouldHaveErrorMessage', message => {
+    cy.get(globalSelectors.errorMessage)
+        .should('exist')
+        .should('contain.text', message)
 })
