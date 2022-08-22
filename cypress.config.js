7767cc702fdd681806f699d9da50d4e2750f361e
Vinai Kopp <vinai@netzarbeiter.com>
2022-08-22 20:31:31 +0200

    Do preflight request to determine specs to run if default spec pattern is used

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,23 @@
 const {defineConfig} = require('cypress');
-const tagify = require("cypress-tags");
-const { existsSync } = require('fs');
-const envConfig = existsSync('./cypress.env.json') ? require('./cypress.env.json') : {};
+const tagify = require('cypress-tags');
+const fs = require('fs');
+const envConfig = fs.existsSync('./cypress.env.json') ? require('./cypress.env.json') : {};
 
 // Run "NODE_ENV=develop; npx cypress run" to run tests locally
-const baseUrl = process.env.NODE_ENV === "develop" ? "http://cypress.magento2.localhost" : "https://example.com/";
+const defaultBaseUrl = process.env.NODE_ENV === 'develop' ? 'http://cypress.magento2.localhost' : 'https://example.com/';
 
 // Sometimes our local envs are slow due to dev mode. Raising the timeout decreases flakyness
-const defaultCommandTimeout = process.env.NODE_ENV === "develop" ? 10000 : 4000;
+const defaultCommandTimeout = process.env.NODE_ENV === 'develop' ? 10000 : 4000;
+
+// Customize to use either hyva, luma or custom specs
+const defaultSpecPattern = 'cypress/integration/**/*.spec.js';
+
+const baseUrl = process.env.CYPRESS_MAGENTO2_BASE_URL || envConfig.MAGENTO2_BASE_URL || defaultBaseUrl;
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: process.env.CYPRESS_MAGENTO2_BASE_URL || envConfig.MAGENTO2_BASE_URL || baseUrl,
-        specPattern: process.env.CYPRESS_MAGENTO2_SPEC_PATTERN || envConfig.MAGENTO2_SPEC_PATTERN ||'cypress/integration/**/*.spec.js',
+        baseUrl: baseUrl,
+        specPattern: process.env.CYPRESS_MAGENTO2_SPEC_PATTERN || envConfig.MAGENTO2_SPEC_PATTERN || defaultSpecPattern,
         excludeSpecPattern: process.env.CYPRESS_MAGENTO2_EXCLUDE_PATTERN || envConfig.MAGENTO2_EXCLUDE_PATTERN || '',
         defaultCommandTimeout: process.env.CYPRESS_MAGENTO2_DEFAULT_TIMEOUT || envConfig.MAGENTO2_DEFAULT_TIMEOUT || defaultCommandTimeout,
         watchForFileChanges: false,
@@ -26,7 +31,23 @@ module.exports = defineConfig({
         },
 
         setupNodeEvents(on, config) {
+
             on('file:preprocessor', tagify(config))
+
+            return config.specPattern !== 'cypress/integration/**/*.spec.js'
+                ? config
+                : new Promise((resolve, reject) => {
+                    // Do a preflight request to determine which test subset to run depending on the target theme
+                    if (baseUrl.startsWith('https')) process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+                    const req = require('https').request(baseUrl);
+                    req.on('response', res => {
+                        const themeSpecs = res.headers['x-built-with'] === 'Hyva Themes' ? 'hyva' : 'luma'
+                        config.specPattern = `cypress/integration/${themeSpecs}/**/*.spec.js`
+                        resolve(config);
+                    })
+                    req.on('error', reject);
+                    req.end();
+                });
         }
     }
 });
